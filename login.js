1d9c89eadf67920d8db3800cfd60de92f8ae0370
unknown <mpanza.vusi5@gmail.com>
2022-01-25 14:06:27 +0200

    .div

---
+++ login.js
@@ -0,0 +1,15 @@
+
+
+function mySelect() {
+    var email = document.forms[ "myform"] ["Uname"].value;
+    var pw = document.forms[ "myform"] ["Uname"].Pass;
+    if(un=="user@gmail.com" && pw=="mypassword");
+}
+
+{
+    window.location.href="Hello welcome my friend.html";
+}
+
+{
+  alert("invali UseName and password");  
+}
