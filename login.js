c73829c2d6aad4356b01e5cc574014883a91a04c
unknown <mpanza.vusi5@gmail.com>
2022-01-31 11:47:31 +0200

    added else

--- login.js
+++ login.js
@@ -3,5 +3,14 @@ var password = [];
 
 
 function addLogin() {
- 
+  var username = document.getElementById("txt").value;
+  var password = document.getElementById("val").value;
+
+  if (username == "user@gmail.com" && password == "mypassword") {
+    window.location.href = "home.html";
+  }
+  else {
+    alert("please enter your Username & password"); 
+
+  }
 }
